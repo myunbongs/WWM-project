9a02e960ff7197e1157c332f7d676e2a49a45815
HyunwooYi <stephenyi28@gmail.com>
2022-08-05 20:27:59 +0900

    user-model-add

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+# 이메일
+    userid =models.CharField(max_length=30, unique = True, verbose_name='email')
+# 주소
+    address = models.CharField(max_length=50, unique = True, verbose_name='address')
+# 이름
+    name = models.CharField(max_length=20, unique = True, verbose_name='name')
+# 가입된 그룹 아이디
+    groupid = models.IntegerField(max_length=20, unique = True, verbose_name='groupid')
