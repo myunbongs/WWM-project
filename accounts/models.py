95351c45e5b321b9e4703261d6badd390fd5a22d
HyunwooYi <stephenyi28@gmail.com>
2022-08-06 14:07:40 +0900

    user_model 수정

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-# 이메일
-    userid =models.CharField(max_length=30, unique = True, verbose_name='email')
 # 주소
     address = models.CharField(max_length=50, unique = True, verbose_name='address')
 # 이름
     name = models.CharField(max_length=20, unique = True, verbose_name='name')
-# 가입된 그룹 아이디
-    groupid = models.IntegerField(max_length=20, unique = True, verbose_name='groupid')
+# 일주일 기준으로 월요일부터 총 24글자씩이라고 가정 
+    avaliablity_days_time: models.CharField(max_length=168)  
