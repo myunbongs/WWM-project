7e87116e5f64edc883bcf29e08baa27e1eace327
myunbongs <62318430+myunbongs@users.noreply.github.com>
2022-08-16 07:32:45 +0900

    feat: ✨ wheremeet 기능 초안 추가(#11) (#25)
    * fix: 🩹 group별 user list 출력
    
    * feat: ✨ wheremeet 기능 초안 추가(#11)

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    address = models.CharField(max_length=50, unique = True, verbose_name='address')
-    name = models.CharField(max_length=20, unique = True, verbose_name='name')
-    avaliablity_days_time = models.TextField()  
+    latitude = models.FloatField(verbose_name='위도')
+    longitude = models.FloatField(verbose_name='경도')
+    name = models.CharField(max_length=20, unique = True, verbose_name='사용자 이름')
+    avaliablity_days_time = models.TextField(verbose_name='meet 가능 시간')  
--- wheremeet/views.py
+++ wheremeet/views.py
@@ -1,16 +1,98 @@
+import json
+import math
+from django.http import JsonResponse
 from django.shortcuts import render
+import pandas as pd
+
+from wwmgroup.models import WwmGroup
+
+def main(reqeust):
+    return render(reqeust, 'wheremeet/coordinate_save.html')
 
 # 참고 로직: https://coding-god.tistory.com/72
 
 # 1. 프론트엔드에서 그룹원별 위치 정보(위도, 경도) 받는 함수  
+def save_coordinate(request):
+    
+    if request.method == 'POST':
+        coordinate = json.loads(request.body)
+
+        current_user = request.user
+        latitude= float(coordinate['latitude'])
+        longitude= float(coordinate['longitude'])
+
+        print(latitude, longitude)
+
+        current_user.latitude = latitude
+        current_user.longitude = longitude
+
+        current_user.save()
+        
+        context={'latitude': current_user.latitude, 'longitude': current_user.longitude}
+    
+    return JsonResponse(context)
 
 # 2. 중간 지점 및 가장 가까운 지하철 역 뿌리는 view 
+def wheremeet_result(request, group_pk):
+
+    wwmgroup = WwmGroup.objects.get(id=group_pk)
+
+    user_list = wwmgroup.user.all()
+
+    latitude, longitude = cal_center(user_list)
+
+
+    station = cal_close_station(latitude, longitude, 'wheremeet/서울시 역사마스터 정보.csv')
+
+    wwmgroup.meeting_station = station['역사명']
+
+    wwmgroup.save()
+
+    context = {
+        'station': wwmgroup.meeting_station,
+        'latitude_station': station['위도'], 
+        'longitude_station': station['경도']
+    }
+
+    return render(request, 'wheremeet/group_station.html', context)
+
 
 # 2-1. 그룹원별 위치 정보(위도, 경도)의 평균 계산 함수 
+def cal_center(user_list):
+
+    latitude = 0.0
+    longitude = 0.0
+
+    for user in user_list:
+        latitude += user.latitude 
+        longitude += user.longitude
+
+    latitude = latitude / user_list.count()
+    longitude = longitude / user_list.count()
+
+    print(latitude, longitude)
+
+    return latitude, longitude
+
 
 # 2-2. 서울시 역사마스터 정보.csv dataframe 형태로 저장
 # - 컬럼: 역사_ID, 역사명, 호선, 위도, 경도 
+def cal_close_station(latitude, longitude, file_name):
+    df = pd.read_csv(file_name, encoding='cp949')
+    df['center_latitude'] = latitude
+    df['center_longitude'] = longitude
+    df['최종 거리'] = float('inf')
 
-# 2-3. 행별로 돌리면서 두 점(중간 지점의 위도 경도, 역 위도 경도) 사이의 거리 계산 및 최솟값 구하기 
+    for i, row in df.iterrows():
+        df['최종 거리'][i] = distance(float(row['center_latitude']), float(row['center_longitude']), float(row['위도']), float(row['경도']))
 
+    station = df.loc[df["최종 거리"].idxmin()]
 
+    print(station)
+
+    return station
+
+# 두 점 사이의 거리 구하기 
+def distance(x1, y1, x2, y2):
+    result = math.sqrt( math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
+    return result
