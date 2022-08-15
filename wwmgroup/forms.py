740916b48e9ae343c15f93d804df68409b5d1f30
myunbongs <62318430+myunbongs@users.noreply.github.com>
2022-08-15 22:49:01 +0900

    Revert "Wwmgroup 앱개발"

--- wwmgroup/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from wwmgroup.models import WwmGroup
-
-
-class groupForm(forms.ModelForm):
-    class Meta:
-        model = WwmGroup
-        fields = ['groupName', 'desc', 'startdate', 'enddate', 'leader_email']
