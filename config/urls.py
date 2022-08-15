69152b96a14eb5ed03d7b9a76634d16b0f8349d1
Hyewon419 <hijjoy@naver.com>
2022-08-16 00:26:43 +0900

    feat: ✨ accounts url 추가(#12)

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('whenmeet.urls')),
+    path('accounts/',include('accounts.urls'))
 ]
