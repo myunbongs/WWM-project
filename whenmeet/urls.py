cad65db395d2fc9ee157cd03de7263453cf2a4c4
jihoon kim <87347699+jomulagy@users.noreply.github.com>
2022-08-17 03:38:09 +0900

    feat: ✨ whenmeet view 수정 (#27)
    * feat: ✨ whenmeet view 수정
    
    * eat: ✨ whenmeet url.py, views.py 작성
    
    개인 시간그래프 수정 url, 함수 추가
    
    * Revert "eat: ✨ whenmeet url.py, views.py 작성"
    
    This reverts commit 2d04d36b5f870e7835339e1f8bf144bc94e770bf.
    
    * feat: ✨ whenmeet url.py views.py 추가
    
    개인시간그래프와 연동

--- whenmeet/urls.py
+++ whenmeet/urls.py
@@ -3,7 +3,7 @@ from . import views
 whenmeet_patterns = [
     path('group_timetable/<int:pk>',views.post_group_timetable,name = "get_group_timetable"),
     path('personal_timetable/',views.post_personal_timetable,name = "get_personal_timetable"),
-
+    path('edit_personal_timetable/',views.edit_personal_timetable,name= "edit_personal_timetable"),
 ]
 
 urlpatterns = [
